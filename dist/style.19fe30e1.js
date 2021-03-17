9aa2a6d68f2c8e481d3e200b21de72201fc90ce4
andy8421 <yichang8421@qq.com>
2021-03-18 00:10:33 +0800

    用 canvas 划线（使用小圆或者小方块叠加）

--- dist/style.19fe30e1.js
+++ dist/style.19fe30e1.js
@@ -165,7 +165,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":6}],13:[function(require,module,exports) {
+},{"_css_loader":6}],21:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -335,4 +335,4 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[13], null)
+},{}]},{},[21], null)
